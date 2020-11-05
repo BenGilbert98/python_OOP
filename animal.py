71d5d4943ce7f23194233684ec42092a5da99328
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-05 11:22:08 +0000

    added Reptile class as a child class for Animal

--- animal.py
+++ animal.py
@@ -1,2 +1,30 @@
 # Creating an Animal class as PARENT / BASE / SUPER class
 
+class Animal:
+
+    def __init__(self):  # initialising the Animal class
+        self.alive = True  # Creating an attribute / variable
+        self.spine = True
+        self.lungs = True
+        self.eyes = True
+
+    # Create behaviours as functions / methods
+    def breathe(self):
+        return "Keep breathing to stay alive"
+
+    def move(self):
+        return "Left to right and up and down ..."
+
+    def eat(self):
+        return "Nom Nom Nom"
+
+    def procreate(self):
+        return "Find partner"
+
+
+# Instantiate our class / create an object
+
+# cat = Animal() # Creating an object of Animal class cat(child class) which has inherited everything from Animal
+# # (# parent class)
+# Abstrcted eat() method from our parent class
+# print(cat.eat())
---
+++ reptile.py
@@ -0,0 +1,34 @@
+# Creating a Reptile class as a child class of Animal class
+
+from animal import Animal  # Syntax to import files and classes
+
+
+class Reptile(Animal):
+    def __init__(self):
+        super().__init__()  # Super key word is used to inherit everything from a parent class
+        self.cold_blooded = True
+        self.tetrapod = True
+        self.heart_chambers = [3, 4]
+
+    # Creating functions for our Reptile class
+
+    def seek_heat(self):
+        return "Look for a warm place"
+
+    def hunt(self):
+        return "Go get that meal"
+
+    def use_venom(self):
+        return "If i've got it i'm using it"
+
+
+# Lets create an object of our Reptile class to utilise the functionalities of OOP
+reptile_object = Reptile()
+
+print(reptile_object.eat()) # Using functions from parent class (Animal) to test the super().__init__()
+print(reptile_object.breathe())
+print(reptile_object.procreate())
+
+print(reptile_object.hunt()) # Using functions from Reptile
+print(reptile_object.seek_heat())
+print(reptile_object.use_venom())
